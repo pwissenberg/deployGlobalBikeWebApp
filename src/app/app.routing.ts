149feb2b335ebd6d353e04a82cc9a00785802857
Henri Wissenberg <henriwissenberg@Henris-MacBook-Air-2.local>
2021-06-26 22:33:34 +0200

    Correction Spelling

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -11,13 +11,13 @@ import { Schritt5Component } from './schritt5/schritt5.component';
 
 const APP_ROUTES = [
     
-    {path: 'ichliebeSAP-S4HANA-viel-Spass-beim-ausprobieren/intro', component: IntroComponent},
-    {path: 'ichliebeSAP-S4HANA-viel-Spass-beim-ausprobieren/impressum', component: ImpressumComponent},
-    {path:'ichliebeSAP-S4HANA-viel-Spass-beim-ausprobieren/schritt-1', component: Schritt1Component},
-    {path:'ichliebeSAP-S4HANA-viel-Spass-beim-ausprobieren/schritt-2', component: Schritt2Component},
-    {path:'ichliebeSAP-S4HANA-viel-Spass-beim-ausprobieren/schritt-3', component: Schritt3Component},
-    {path:'ichliebeSAP-S4HANA-viel-Spass-beim-ausprobieren/schritt-4', component: Schritt4Component},
-    {path:'ichliebeSAP-S4HANA-viel-Spass-beim-ausprobieren/schritt-5', component: Schritt5Component},
-    { path: '', component: ContactComponent}
+    {path: 'intro', component: IntroComponent},
+    {path: 'impressum', component: ImpressumComponent},
+    {path:'schritt-1', component: Schritt1Component},
+    {path:'schritt-2', component: Schritt2Component},
+    {path:'schritt-3', component: Schritt3Component},
+    {path:'schritt-4', component: Schritt4Component},
+    {path:'schritt-5', component: Schritt5Component},
+    { path: '', component: IntroComponent}
 ];
 export const routing = RouterModule.forRoot(APP_ROUTES);
