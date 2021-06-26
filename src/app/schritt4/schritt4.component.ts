667edd65279a97753cdfed2368c903f419dde5a9
Henri Wissenberg <henriwissenberg@Henris-MacBook-Air-2.local>
2021-06-26 18:18:22 +0200

    first commit

---
+++ src/app/schritt4/schritt4.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schritt4',
+  templateUrl: './schritt4.component.html',
+  styles: [
+  ]
+})
+export class Schritt4Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/schritt5/schritt5.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-schritt5',
+  templateUrl: './schritt5.component.html',
+  styles: [
+  ]
+})
+export class Schritt5Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
